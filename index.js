1d0d7b04ddfd87d2cb42fbbda148bc3111f264a0
Anu9969 <sharmaanurag9969@gmail.com>
2024-12-02 00:05:32 +0530

    sum of n numbers update

--- index.js
+++ index.js
@@ -3,8 +3,20 @@ const express = require('express');
 
 const app = express();   //this is like creating my clinic 
 
+function sum(n){
+    let ans = 0;
+    for(let  i = 0; i<n ;i++){
+        ans= ans+i;
+        
+    }
+    return ans;
+}
+
 app.get('/', function(req,res){
-    res.send('Hello world')
+    const n =  req.query.n;
+    let a = sum(n);
+    res.send('The sum of the first '+n+' numbers is '+a);
+
 })
 
 app.listen(3000)   //this is like giving room munber
